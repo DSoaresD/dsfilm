c8d6afe24cbbe6af07060eb2be154730ec289f98
Daniel Soares <danielsoares321@gmail.com>
2022-03-11 17:15:51 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsfilm/DsfilmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsfilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsfilmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsfilm/DsfilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsfilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
