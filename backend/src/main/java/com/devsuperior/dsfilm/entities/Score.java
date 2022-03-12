d424d3fe28290d69ac5daf8aea9dfbec993cb730
Daniel Soares <danielsoares321@gmail.com>
2022-03-12 02:23:34 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsfilm/entities/Score.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dsfilm.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	
+	//em chaves compostas é necessario instanciar para garantir sua instanciação
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	
+	private Double value;
+	
+	public Score() {
+		
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
+	
+}
